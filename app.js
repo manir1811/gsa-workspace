81073ecc67c40362d699cee2a42f92c721953baa
Manikandan Rajagopal <manir1811@gmail.com>
2018-03-16 11:08:18 +0000

    Auth added

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var Job = require("./models/job");
 mongoose.connect("mongodb://localhost/workspace");
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/public'));
 app.use(methodOverride("_method"));
 
 
@@ -27,6 +28,11 @@ passport.use(new localStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+});
+
 //=========== JOB ROUTES ================
 //=======================================
 
@@ -36,20 +42,20 @@ app.get("/", function(req, res){
 
 // JOB SHOW ALL
 
-app.get("/jobs", function(req,res){
+app.get("/jobs", isLoggedIn, function(req,res){
     //Find all the jobs from DB and loop it
     Job.find({},function(err, alljobs){
         if(err){
             console.log(err);
         } else {
-            res.render("jobs",{jobs:alljobs}); 
+            res.render("jobs/jobs",{jobs:alljobs}); 
         }
     });
 });
 
 // JOB CREATE
 
-app.post("/jobs", function(req, res){
+app.post("/jobs", isLoggedIn, function(req, res){
     // Get the form data and add it into the array for now!
     
     var IMO = req.body.job.IMO;
@@ -86,32 +92,32 @@ app.post("/jobs", function(req, res){
 
 // JOB NEW FORM
 
-app.get("/jobs/new", function(req, res){
-    res.render("job-new");
+app.get("/jobs/new", isLoggedIn, function(req, res){
+    res.render("jobs/job-new");
 });
 
 // JOB DETAILS PAGE
 
-app.get("/jobs/:id", function(req, res){
+app.get("/jobs/:id", isLoggedIn, function(req, res){
     // FIND THE JOB DETAILS WITH PROVIDED ID
     Job.findById(req.params.id).populate("ship").exec(function(err, foundjobDetails){
         if(err){
             console.log(err);
         } else {
             // console.log(foundjobDetails);
-            res.render("job-details", {jobDetails: foundjobDetails});
+            res.render("jobs/job-details", {jobDetails: foundjobDetails});
         }
     });
 });
 
 // JOB EDIT
 
-app.get("/jobs/:id/edit", function(req, res){
+app.get("/jobs/:id/edit", isLoggedIn, function(req, res){
     Job.findById(req.params.id, function(err, foundjobDetails){
         if(err){
             console.log(err);
         } else {
-                res.render("job-edit", {jobDetails: foundjobDetails});
+                res.render("jobs/job-edit", {jobDetails: foundjobDetails});
                 // console.log(foundjobDetails);
         }
     });
@@ -119,7 +125,7 @@ app.get("/jobs/:id/edit", function(req, res){
 
 // JOB UPDATE
 
-app.put("/jobs/:id", function(req, res){
+app.put("/jobs/:id", isLoggedIn, function(req, res){
     Job.findByIdAndUpdate(req.params.id, req.body.job, function(err, updatedJobDetails){
        if(err){
            console.log(err);
@@ -131,7 +137,7 @@ app.put("/jobs/:id", function(req, res){
 });
 
 // JOB DELETE
-app.delete("/jobs/:id", function(req, res){
+app.delete("/jobs/:id", isLoggedIn, function(req, res){
     Job.findByIdAndRemove(req.params.id, function(err){
         if(err){
             console.log(err);
@@ -145,12 +151,12 @@ app.delete("/jobs/:id", function(req, res){
 //============== SHIP ROUTES ====================
 //===============================================
 
-app.get("/ships", function(req, res){
+app.get("/ships", isLoggedIn, function(req, res){
     Ship.find({}, function(err, allships){
         if(err){
             console.log(err);
         } else {
-            res.render("ships",{ships:allships});
+            res.render("ships/ships",{ships:allships});
         }
     });
     
@@ -158,13 +164,13 @@ app.get("/ships", function(req, res){
 
 // SHIP - NEW - FORM
 
-app.get("/ships/new", function(req, res){
-    res.render("ship-new");
+app.get("/ships/new", isLoggedIn, function(req, res){
+    res.render("ships/ship-new");
 });
 
 // SHIP DETAILS PAGE
 
-app.post("/ships", function(req, res){
+app.post("/ships", isLoggedIn, function(req, res){
     Ship.create(req.body.ship, function(err, newCreatedShip){
         if(err){
             console.log(err);
@@ -178,25 +184,25 @@ app.post("/ships", function(req, res){
 
 // SHIP DETAILS
 
-app.get("/ships/:id", function(req, res){
+app.get("/ships/:id", isLoggedIn, function(req, res){
     // FIND THE SHIP DETAILS WITH PROVIDED ID
     Ship.findById(req.params.id,function(err, foundShipDetails){
         if(err){
             console.log(err);
         } else {
-            res.render("ship-details", {shipDetails: foundShipDetails});
+            res.render("ships/ship-details", {shipDetails: foundShipDetails});
         }
     });
 });
 
 // SHIP EDIT
 
-app.get("/ships/:id/edit", function(req, res){
+app.get("/ships/:id/edit", isLoggedIn, function(req, res){
     Ship.findById(req.params.id, function(err, foundShipDetails){
         if(err){
             console.log(err);
         } else {
-                res.render("ship-edit", {shipDetails: foundShipDetails});
+                res.render("ships/ship-edit", {shipDetails: foundShipDetails});
                 // console.log(foundShipDetails);
         }
     });
@@ -204,7 +210,7 @@ app.get("/ships/:id/edit", function(req, res){
 
 // SHIP UPDATE
 
-app.put("/ships/:id", function(req, res){
+app.put("/ships/:id", isLoggedIn, function(req, res){
     Ship.findByIdAndUpdate(req.params.id, req.body.ship, function(err, updatedShipDetails){
        if(err){
            console.log(err);
@@ -217,7 +223,7 @@ app.put("/ships/:id", function(req, res){
 
 // SHIP DELETE
 
-app.delete("/ships/:id", function(req, res){
+app.delete("/ships/:id", isLoggedIn, function(req, res){
     Ship.findByIdAndRemove(req.params.id, function(err){
         if(err){
             console.log(err);
@@ -232,13 +238,13 @@ app.delete("/ships/:id", function(req, res){
 
 // SHOW REGISTER FORM
 
-app.get('/register', function(req, res){
+app.get('/register', isLoggedIn, function(req, res){
     res.render('register');
 });
 
 // HANDLE SIGN UP LOGIC
 
-app.post('/register', function(req, res){
+app.post('/register', isLoggedIn, function(req, res){
     var newUser = new User({username: req.body.username});
     User.register(newUser, req.body.password, function(err, user){
         if(err){
@@ -275,6 +281,15 @@ app.get('/logout', function(req, res){
     req.logout();
     res.redirect('/login');
 });
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    } else {
+        res.redirect('/login');
+    }
+}
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("GSA Workspace server has started!");
